0396ed0b26633c88eb28f40b8398962954b519dd
MeHigh22 <pmihai31@gmail.com>
2021-04-27 09:28:43 +0200

    Push

---
+++ script.js
@@ -0,0 +1,11 @@
+// let eleves = ["Alexis", "Andy", "Charles", "Dorian", "Farhad", "Ilyas", "Junior", "Kev", "Loïc", "Mihai", "Oussama", "Tania", "William", "Alain"];
+
+// for(let i = 0; i < eleves.length; i++){
+//     console.log(`Bonjour ${eleves[i]} `);
+// }
+
+let nbr = +prompt("Donne un chiffre");
+
+for (let i = 0; i <= nbr ; i++) {
+    console.log(`${number} * ${i} = ${result}`)
+}
